fc7fb012acfdd4c1ed0db06c790ca3a38420e1c7
BlueShiftButterfly <34643088+BlueShiftButterfly@users.noreply.github.com>
2024-03-21 18:52:31 +0200

    Added signup page and changed login page

--- application/blueprints/authentication.py
+++ application/blueprints/authentication.py
@@ -1,7 +1,22 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 
 blueprint = Blueprint("authentication", __name__, url_prefix="/auth")
 
-@blueprint.route("/login")
+@blueprint.route("/login", methods=("GET", "POST"))
 def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        return redirect(url_for("index"))
+
     return render_template("user_login.html")
+
+@blueprint.route("/signup", methods=("GET", "POST"))
+def signup():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        email = request.form.get("email")
+        return redirect(url_for("index"))
+
+    return render_template("user_signup.html")
