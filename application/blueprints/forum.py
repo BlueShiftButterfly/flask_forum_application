66328a847a8bae463806f9c2b253ab18023d7baf
BlueShiftButterfly <34643088+BlueShiftButterfly@users.noreply.github.com>
2024-04-19 18:36:52 +0300

    Admins can now create forums

--- application/blueprints/forum.py
+++ application/blueprints/forum.py
@@ -1,7 +1,8 @@
 from flask import Blueprint
-from application.views import forum
+from application.views import forum, forum_create
 
 class ForumBlueprint:
     def __init__(self, db) -> None:
-        self.blueprint = Blueprint("forum", __name__, url_prefix="/forum")
-        self.blueprint.add_url_rule("/<forum_name>", view_func=forum.ForumView.as_view("forum_view", "forum.html", db))
+        self.blueprint = Blueprint("forum", __name__)
+        self.blueprint.add_url_rule("/forum/<forum_name>", view_func=forum.ForumView.as_view("forum_view", "forum.html", db))
+        self.blueprint.add_url_rule("/create_forum", view_func=forum_create.ForumCreateView.as_view("forum_create_view", "forum_create.html", db))
---
+++ application/views/forum_create.py
@@ -0,0 +1,25 @@
+from flask.views import View
+from flask import render_template, request, abort, redirect
+from flask_login import current_user, login_required
+from application.db import DatabaseBridge
+from application.timestamp import get_date_from_timestamp
+from application.permissions import check_permissions_forum, ContentAction
+
+class ForumCreateView(View):
+    methods = ["GET", "POST"]
+    def __init__(self, template: str, db: DatabaseBridge) -> None:
+        self.template = template
+        self.db = db
+
+    @login_required
+    def dispatch_request(self):
+        print(current_user.role)
+        if request.method == "GET" and check_permissions_forum(current_user, ContentAction.CREATE):
+            return render_template(self.template)
+        if request.method == "POST" and check_permissions_forum(current_user, ContentAction.CREATE):
+            forum_url_name = request.form.get("forum_url_name")
+            forum_name = request.form.get("forum_name")
+            forum_description = request.form.get("forum_description")
+            self.db.create_forum(forum_url_name, forum_name, forum_description, current_user.db_id)
+            return redirect(f"/forum/{forum_url_name}")
+        abort(404)
