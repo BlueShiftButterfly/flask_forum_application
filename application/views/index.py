4e98ba02a82d2f1367cd2272f82f2e6c7f768465
BlueShiftButterfly <34643088+BlueShiftButterfly@users.noreply.github.com>
2024-05-01 21:48:43 +0300

    Private forums no longer show up in frontpage unless accessible

--- application/views/index.py
+++ application/views/index.py
@@ -12,5 +12,6 @@ class IndexView(View):
 
     def dispatch_request(self):
         forums = self.db.get_all_forums()
-        return render_template(self.template, forums=forums, can_create_forum=check_permissions_forum(current_user, ContentAction.CREATE), forum_create_link=url_for("forum.forum_create_view"))
+        viewable_forums = [forum for forum in forums if forum.is_invite_only == False or (forum.is_invite_only and current_user.username in forum.invited_users)]
+        return render_template(self.template, forums=viewable_forums, can_create_forum=check_permissions_forum(current_user, ContentAction.CREATE), forum_create_link=url_for("forum.forum_create_view"))
     
